56ea2286cf6456f86f3e7ded2a44ea18b12408d1
liyy <1290374862@qq.com>
2019-12-30 23:13:46 +0800

    丢帧以至同步

--- ijkplayer-java/src/main/java/tv/danmaku/ijk/media/widget/media/IjkVideoView.java
+++ ijkplayer-java/src/main/java/tv/danmaku/ijk/media/widget/media/IjkVideoView.java
@@ -1033,8 +1033,10 @@ public class IjkVideoView extends FrameLayout implements MediaController.MediaPl
                     ijkMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "packet-buffering", 0);
                     ijkMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "fflags","nobuffer");
 
+                    // framedrop 是在视频帧处理不过来的时候丢弃一些帧达到同步的效果
+                    ijkMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "framedrop", 30);
+
 //                    ijkMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "iformat", "hls");
-//                    ijkMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "framedrop", 30);
 //                    ijkMediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "flush_packets", 1);
 
 //                    pause output until enough packets have been read after stalling
